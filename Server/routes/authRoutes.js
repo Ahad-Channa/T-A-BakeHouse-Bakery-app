deb837bec645b7c8ebf6f91ba042bce38a97f762
Ahad-Channa <ahadchanna45@gmail.com>
2025-06-09 01:16:01 -0700

    start working on server Login and registiration

---
+++ Server/routes/authRoutes.js
@@ -0,0 +1,10 @@
+import express from 'express'
+
+const router = express.Router()
+
+import { register, login } from '../controllers/authController'
+
+router.post('/register', register);
+router.post('/login', login);
+
+module.exports = router;
